2ee821d0c7d28f65776f00a6d0eec18421d1e3e7
Sean Molenaar <sean@m2mobi.com>
2022-04-26 14:38:31 +0200

    Core: Add PHPStan extension for ServiceLocator
    Issue MM-5
    
    Reviewed at https://reviews.lunr.nl/r/829/

---
+++ src/Lunr/Core/PHPStan/ConfigServiceLocatorRecipeReflection.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace Lunr\Core\PHPStan;
+
+use PHPStan\Reflection\ClassMemberReflection;
+use PHPStan\Reflection\ClassReflection;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Reflection\TrivialParametersAcceptor;
+use PHPStan\TrinaryLogic;
+
+/**
+ * Methodreflection for ConfigServiceLocator recipes
+ * @see https://phpstan.org/developing-extensions/class-reflection-extensions
+ */
+class ConfigServiceLocatorRecipeReflection implements MethodReflection
+{
+
+    /**
+     * The class that declared the method
+     *
+     * @var ClassReflection
+     */
+    private $declaringClass;
+
+    /**
+     * Name of the method
+     *
+     * @var string
+     */
+    private $name;
+
+    /**
+     * Construct a MethodReflection
+     *
+     * @param ClassReflection $declaringClass The class that declared the method
+     * @param string          $name           Name of the method
+     */
+    public function __construct(ClassReflection $declaringClass, string $name)
+    {
+        $this->declaringClass = $declaringClass;
+        $this->name           = $name;
+    }
+
+    /**
+     * The class that declared the method
+     * @return ClassReflection
+     */
+    public function getDeclaringClass(): ClassReflection
+    {
+        return $this->declaringClass;
+    }
+
+    /**
+     * Is the method static
+     * @return bool
+     */
+    public function isStatic(): bool
+    {
+        return FALSE;
+    }
+
+    /**
+     * Is the method private
+     * @return bool
+     */
+    public function isPrivate(): bool
+    {
+        return FALSE;
+    }
+
+    /**
+     * Is the method public
+     * @return bool
+     */
+    public function isPublic(): bool
+    {
+        return TRUE;
+    }
+
+    /**
+     * Get the name of the method
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get the method prototype
+     * @return ClassMemberReflection
+     */
+    public function getPrototype(): ClassMemberReflection
+    {
+        return $this;
+    }
+
+    /**
+     * Get the variants of the parameters to accept
+     * @return \PHPStan\Reflection\ParametersAcceptor[]
+     */
+    public function getVariants(): array
+    {
+        return [
+            new TrivialParametersAcceptor(),
+        ];
+    }
+
+    /**
+     * Get the doc comment for the method
+     * @return string|null
+     */
+    public function getDocComment(): ?string
+    {
+        return NULL;
+    }
+
+    /**
+     * Is the method deprecated
+     * @return TrinaryLogic
+     */
+    public function isDeprecated(): \PHPStan\TrinaryLogic
+    {
+        return TrinaryLogic::createNo();
+    }
+
+    /**
+     * Get the description why the method is deprecated
+     * @return string|null
+     */
+    public function getDeprecatedDescription(): ?string
+    {
+        return NULL;
+    }
+
+    /**
+     * Is the method final
+     * @return TrinaryLogic
+     */
+    public function isFinal(): \PHPStan\TrinaryLogic
+    {
+        return TrinaryLogic::createNo();
+    }
+
+    /**
+     * Is the method internal
+     * @return TrinaryLogic
+     */
+    public function isInternal(): \PHPStan\TrinaryLogic
+    {
+        return TrinaryLogic::createNo();
+    }
+
+    /**
+     * Get the type of exceptions to throw
+     * @return \PHPStan\Type\Type|null
+     */
+    public function getThrowType(): ?\PHPStan\Type\Type
+    {
+        return NULL;
+    }
+
+    /**
+     * Does the method have side effects
+     * @return TrinaryLogic
+     */
+    public function hasSideEffects(): \PHPStan\TrinaryLogic
+    {
+        return TrinaryLogic::createNo();
+    }
+
+}
---
+++ src/Lunr/Core/PHPStan/ConfigServiceLocatorRecipeReflectionExtension.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Lunr\Core\PHPStan;
+
+use Lunr\Core\ConfigServiceLocator;
+use PHPStan\Reflection\ClassReflection;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Reflection\MethodsClassReflectionExtension;
+
+/**
+ * Method reflection extension for ConfigServiceLocator recipes
+ * @see https://phpstan.org/developing-extensions/class-reflection-extensions
+ */
+class ConfigServiceLocatorRecipeReflectionExtension implements MethodsClassReflectionExtension
+{
+    /**
+     * List of locators that are pre-set
+     * @var array
+     */
+    private array $presetList = [ 'config' ];
+
+    /**
+     * Constructor for ConfigServiceLocatorRecipeReflectionExtension.
+     *
+     * @param array $presetList List of pre-set locator recipes.
+     */
+    public function __construct(array $presetList = [])
+    {
+        $this->presetList = array_merge($this->presetList, $presetList);
+    }
+
+    /**
+     * Destructor.
+     */
+    public function __destruct()
+    {
+        unset($this->presetList);
+    }
+
+    /**
+     * Check if the recipe exists or is one of the special cases
+     *
+     * @param  ClassReflection $classReflection The class to check
+     * @param  string          $methodName      Name of the method
+     *
+     * @return bool if the method is present in the class
+     */
+    public function hasMethod(ClassReflection $classReflection, string $methodName): bool
+    {
+        if ($classReflection->getName() !== ConfigServiceLocator::class)
+        {
+            return FALSE;
+        }
+
+        if (in_array($methodName, $this->presetList))
+        {
+            return TRUE;
+        }
+
+        return stream_resolve_include_path('locator/locate.' . $methodName . '.inc.php') !== FALSE;
+    }
+
+    /**
+     * Get a reflection of the class
+     *
+     * @param  ClassReflection $classReflection The class of the reflection
+     * @param  string          $methodName      Name of the reflected method
+     *
+     * @return MethodReflection The reflected method
+     */
+    public function getMethod(ClassReflection $classReflection, string $methodName): MethodReflection
+    {
+        return new ConfigServiceLocatorRecipeReflection($classReflection, $methodName);
+    }
+
+}
